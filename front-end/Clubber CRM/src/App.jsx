f0b44db150b6b64e36649bc5f1182fd6070a51df
K.Surendraraj <surenkworkz@gmail.com>
2023-08-18 22:08:47 +0800

    Added Display to App.jsx

--- front-end/Clubber CRM/src/App.jsx
+++ front-end/Clubber CRM/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import SignIn from "./LoginSignUp/Login";
 import SignUp from "./LoginSignUp/Register";
+import Display from "./Display";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/login" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
       </Routes>
+      <Display></Display>
     </BrowserRouter>
   );
 }
