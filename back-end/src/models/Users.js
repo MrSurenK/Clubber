6b02a3ae3dd0418c3029dc946e2681f48858c482
Firdaus_87 <mohamedf2_net@hotmail.com>
2023-08-18 13:35:24 +0800

    Update Registration

--- back-end/src/models/Users.js
+++ back-end/src/models/Users.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, minLength: 1, maxLength: 50 },
+    hash: { type: String, require: true },
     isActive: { type: Boolean, default: false },
     name: { type: String, required: true, minLength: 1, maxLength: 50 },
     created_at: { type: Date, default: Date.now },
-    hash: { type: String, require: true },
     isStaff: { type: Boolean, default: false },
-    staffId: { type: String, required: true, minLength: 1, maxLength: 50 },
-    staffRank: { type: String, required: true },
+    staffId: { type: String, required: false, minLength: 1, maxLength: 50 },
+    staffRank: { type: String, required: false },
     isMember: { type: Boolean, default: false },
-    memberId: { type: String, required: true, minLength: 1, maxLength: 50 },
-    memberRank: { type: String, require: true },
+    memberId: { type: String, required: false, minLength: 1, maxLength: 50 },
+    memberRank: { type: String, require: false },
     barTabActive: { type: Boolean, default: false },
   },
   { collection: "users" }
