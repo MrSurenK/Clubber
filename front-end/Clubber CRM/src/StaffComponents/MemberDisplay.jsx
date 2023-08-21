c1ff71ce76c709e97947bb9e22d8b1bb586e8b1e
MrSurenK <surenkworkz@gmail.com>
2023-08-21 22:11:47 +0800

    Fixed Transaction not loading due to async

--- front-end/Clubber CRM/src/StaffComponents/MemberDisplay.jsx
+++ front-end/Clubber CRM/src/StaffComponents/MemberDisplay.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import useFetch from "../hooks/useFetch";
-import { ThemeProvider, Typography, TextField } from "@mui/material";
+import {
+  Typography,
+  TextField,
+  Paper,
+  Box,
+  CircularProgress,
+} from "@mui/material";
 import Container from "@mui/material/Container";
 import Table from "@mui/material/Table";
 import TableCell from "@mui/material/TableCell";
@@ -12,6 +18,9 @@ import TableBody from "@mui/material/TableBody";
 const MemberDisplay = () => {
   const [members, setMembers] = useState([]);
   const [search, setSearch] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+  // Due to async nature of transaction call, require loading for table
+  const [loading, setLoading] = useState(false);
 
   const fetchData = useFetch();
   // Get members from API
@@ -26,9 +35,37 @@ const MemberDisplay = () => {
     }
   };
 
+  const getTransactions = async (memberId) => {
+    const res = await fetchData("/transactions/totalamount/" + memberId, "GET");
+    console.log("Transaction response for", memberId, ":", res);
+    if (res.ok) {
+      setTransactions((prevTransactions) => ({
+        ...prevTransactions,
+        [memberId]: res.data.totalAmount,
+      }));
+      console.log(res.data);
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
+
+  const fetchTable = async () => {
+    await Promise.all(
+      members.map((member) => getTransactions(member.memberId))
+    );
+    setLoading(false); // Set loading to false after all transactions are fetched
+  };
+
   useEffect(() => {
     getMembers();
   }, []);
+
+  useEffect(() => {
+    fetchTable();
+  }, [members]); // Depend on members instead of transactions
+
+  console.log("Transaction state: ", transactions);
 
   const handleSearch = () => {
     return members.filter((member) => {
@@ -51,51 +88,62 @@ const MemberDisplay = () => {
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
         ></TextField>
 
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                {[
-                  "Name",
-                  "Member ID",
-                  "email",
-                  "Rank",
-                  "Total Spend",
-                  "Outstanding Amount",
-                ].map((header) => (
-                  <TableCell
-                    sx={{
-                      color: "black",
-                      fontWeight: "700",
-                      fontFamily: "Exo",
-                    }}
-                    key={header}
-                  >
-                    {header}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {handleSearch().map((row) => {
-                return (
-                  <TableRow
-                    sx={{
-                      cursor: "pointer",
-                    }}
-                    key={row.name}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.name}
+        <TableContainer component={Paper}>
+          {loading ? (
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              <CircularProgress sx={{ color: "purple" }} />
+            </Box>
+          ) : (
+            <Table>
+              <TableHead sx={{ bgcolor: "#ffc400" }}>
+                <TableRow>
+                  {[
+                    "Name",
+                    "Member ID",
+                    "Email",
+                    "Rank",
+                    "Total Spend",
+                    "Outstanding Amount",
+                  ].map((header) => (
+                    <TableCell
+                      sx={{
+                        color: "black",
+                        fontWeight: "700",
+                        fontFamily: "Exo",
+                      }}
+                      key={header}
+                    >
+                      {header}
                     </TableCell>
-                    <TableCell>{row.memberId}</TableCell>
-                    <TableCell>{row.email}</TableCell>
-                    <TableCell>{row.memberRank}</TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {handleSearch().map((row) => {
+                  return (
+                    <TableRow
+                      sx={{
+                        cursor: "pointer",
+                      }}
+                      key={row.name}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell>{row.memberId}</TableCell>
+                      <TableCell>{row.email}</TableCell>
+                      <TableCell>{row.memberRank}</TableCell>
+                      <TableCell>
+                        {transactions[row.memberId]
+                          ? transactions[row.memberId]
+                          : "No transactions"}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          )}
         </TableContainer>
       </Container>
     </>
