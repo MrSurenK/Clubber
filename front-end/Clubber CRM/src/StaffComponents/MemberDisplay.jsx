c9514caa717438951530db4deb7a1ae014423366
MrSurenK <surenkworkz@gmail.com>
2023-08-21 20:42:07 +0800

    Fixed search function

--- front-end/Clubber CRM/src/StaffComponents/MemberDisplay.jsx
+++ front-end/Clubber CRM/src/StaffComponents/MemberDisplay.jsx
@@ -34,8 +34,7 @@ const MemberDisplay = () => {
     return members.filter((member) => {
       return (
         member.name.toLowerCase().includes(search) ||
-        member.memberId.includes(search) ||
-        // Excluded member.
+        member.memberId.toLowerCase().includes(search) ||
         member.email.toLowerCase().includes(search)
       );
     });
@@ -43,72 +42,62 @@ const MemberDisplay = () => {
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          marginLeft: "20%",
-        }}
-      >
-        <Container fixed sx={{ textAlign: "center", marginTop: "35em" }}>
-          <Typography variant="h4">Member List</Typography>
-          <TextField
-            label="Search Member..."
-            variant="outlined"
-            sx={{ mb: 5, width: "40%" }}
-            onChange={(e) => setSearch(e.target.value.toLowerCase())}
-          ></TextField>
+      <Container fixed sx={{ textAlign: "center" }}>
+        <Typography variant="h4">Member List</Typography>
+        <TextField
+          label="Search Member..."
+          variant="outlined"
+          sx={{ mb: 5, width: "40%" }}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        ></TextField>
 
-          <TableContainer sx={{ textAlign: "center", marginBottom: "50vh" }}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  {[
-                    "Member ID",
-                    "Name",
-                    "email",
-                    "Rank",
-                    "Total Spend",
-                    "Outstanding Amount",
-                  ].map((header) => (
-                    <TableCell
-                      sx={{
-                        color: "black",
-                        fontWeight: "700",
-                        fontFamily: "Exo",
-                      }}
-                      key={header}
-                    >
-                      {header}
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {[
+                  "Name",
+                  "Member ID",
+                  "email",
+                  "Rank",
+                  "Total Spend",
+                  "Outstanding Amount",
+                ].map((header) => (
+                  <TableCell
+                    sx={{
+                      color: "black",
+                      fontWeight: "700",
+                      fontFamily: "Exo",
+                    }}
+                    key={header}
+                  >
+                    {header}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {handleSearch().map((row) => {
+                return (
+                  <TableRow
+                    sx={{
+                      cursor: "pointer",
+                    }}
+                    key={row.name}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.name}
                     </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {handleSearch().map((row) => {
-                  return (
-                    <TableRow
-                      sx={{
-                        cursor: "pointer",
-                      }}
-                      key={row.name}
-                    >
-                      <TableCell component="th" scope="row">
-                        {row.name}
-                      </TableCell>
-                      <TableCell>{row.memberId}</TableCell>
-                      <TableCell>{row.email}</TableCell>
-                      <TableCell>{row.memberRank}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Container>
-      </div>
+                    <TableCell>{row.memberId}</TableCell>
+                    <TableCell>{row.email}</TableCell>
+                    <TableCell>{row.memberRank}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
     </>
   );
 };
