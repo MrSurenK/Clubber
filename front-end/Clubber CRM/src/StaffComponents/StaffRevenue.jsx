6af9d2e43989d3657e0cd6686542f98fd701dee0
wallgecko <melissa.ho@outlook.com>
2023-08-22 01:18:16 +0800

    completed bar tab with search by transactionID or member name. payment status update also done

--- front-end/Clubber CRM/src/StaffComponents/StaffRevenue.jsx
+++ front-end/Clubber CRM/src/StaffComponents/StaffRevenue.jsx
@@ -5,6 +5,8 @@ import UserContext from "../context/user";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+
 import {
   Table,
   TableHead,
@@ -17,51 +19,124 @@ import {
 const StaffRevenue = () => {
   // const userCtx = useContext(UserContext);
   const [transactions, setTransactions] = useState([]);
-  const fetchData = useFetch();
-  // const transactionIdRef = useRef("");
-  // const transactionDateRef = useRef("");
-  // const paymentStatusRef = useRef("");
-  // const productIdRef = useRef("");
-  // const memberIdRef = useRef("");
-  // const staffIdRef = useRef("");
+  const [products, setProducts] = useState([]);
+  const [staff, setStaff] = useState([]);
+  const [members, setMembers] = useState([]);
 
+  // for pagination
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const fetchData = useFetch();
+
+  // for adding new transaction
+  const transactionIdRef = useRef("");
+  const transactionDateRef = useRef("");
+  const paymentStatusRef = useRef("");
+  const productIdRef = useRef("");
+  const memberIdRef = useRef("");
+  const staffIdRef = useRef("");
+
+  // for updating payment status
+  const [updatedPaymentStatus, setUpdatedPaymentStatus] = useState([]);
+
+  // GET for all transactions
   const getTransactions = async () => {
     const res = await fetchData("/transactions");
 
     if (res.ok) {
       setTransactions(res.data);
+      // Fetch product data
+      const productRes = await fetchData("/products");
+      if (productRes.ok) {
+        setProducts(productRes.data); // Assuming products are stored in state using `setProducts`
+      }
+      // Fetch staff data
+      const staffRes = await fetchData("/users/staff");
+      if (staffRes.ok) {
+        setStaff(staffRes.data); // Assuming staff members are stored in state using `setStaff`
+      }
+      // Fetch member data
+      const memberRes = await fetchData("/users/member");
+
+      if (memberRes.ok) {
+        setMembers(memberRes.data); // Assuming members are stored in state using `setMembers`
+      }
     } else {
       alert(JSON.stringify(res.data));
       console.log(res.data);
     }
   };
 
-  // const addTransactions = async () => {
-  //   const res = await fetchData(
-  //     "/transactions",
-  //     "PUT",
-  //     {
-  //       transactionId: transactionIdRef.current.value,
-  //       transactionDate: transactionDateRef.current.value,
-  //       paymentStatus: paymentStatusRef.current.value,
-  //       productId: productIdRef.current.value,
-  //       memberId: memberIdRef.current.value,
-  //       staffId: staffIdRef.current.value,
-  //     },
-  //     userCtx.accessToken
-  //   );
+  // PATCH to update payment status
+  const handlePaymentStatusChange = async (transaction) => {
+    const updatedStatus = !transaction.paymentStatus; // Toggle the payment status
+    console.log(transaction.transactionId);
+    const res = await fetchData(
+      `/transactions/${transaction.transactionId}`,
+      "PATCH",
+      { paymentStatus: updatedStatus }
+    );
+    if (res.ok) {
+      getTransactions();
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
 
-  //   if (res.ok) {
-  //     getTransactions();
-  //   } else {
-  //     alert(JSON.stringify(res.data));
-  //     console.log(res.data);
-  //   }
-  // };
+  // PUT to add a new transaction
+  const addTransactions = async () => {
+    const res = await fetchData(
+      "/transactions",
+      "PUT",
+      {
+        transactionId: transactionIdRef.current.value,
+        transactionDate: transactionDateRef.current.value,
+        paymentStatus: paymentStatusRef.current.value,
+        productId: productIdRef.current.value,
+        memberId: memberIdRef.current.value,
+        staffId: staffIdRef.current.value,
+      },
+      userCtx.accessToken
+    );
+
+    if (res.ok) {
+      getTransactions();
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
 
   useEffect(() => {
     getTransactions();
   }, []);
+
+  // pagination
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 5));
+    setPage(0);
+  };
+
+  // search query
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredTransactions = transactions.filter((transaction) => {
+    const lowerSearchQuery = searchQuery.toLowerCase();
+    const matchesTransactionId = transaction.transactionId
+      .toLowerCase()
+      .includes(lowerSearchQuery);
+    const member = members.find((m) => m.memberId === transaction.memberId);
+    const matchesMemberName =
+      member && member.name.toLowerCase().includes(lowerSearchQuery);
+
+    return matchesTransactionId || matchesMemberName;
+  });
 
   return (
     <>
@@ -71,31 +146,89 @@ const StaffRevenue = () => {
           sx={{ textAlign: "center", marginBottom: "50vh" }}
         >
           <Button>Add New Transaction</Button>
-          <Typography variant="h5">Existing Transactions</Typography>
+
+          <Typography variant="h5">Open Bar Tab</Typography>
+
+          <input
+            type="text"
+            placeholder="Search by Transaction ID or Member Name"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+
           <Table>
             <TableHead>
               <TableRow>
                 <TableCell>Transaction ID</TableCell>
                 <TableCell>Transaction Date</TableCell>
-                <TableCell>Payment Status</TableCell>
-                <TableCell>Product ID</TableCell>
-                <TableCell>Staff ID</TableCell>
-                <TableCell>Member ID</TableCell>
+                <TableCell>Product Name</TableCell>{" "}
+                <TableCell>
+                  Product Amount
+                  <br /> (S$)
+                </TableCell>
+                <TableCell>Staff Name</TableCell>
+                <TableCell>Member Name</TableCell>{" "}
+                <TableCell>Payment Collected?</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {transactions.map((transaction) => (
-                <TableRow key={transaction.transactionId}>
-                  <TableCell>{transaction.transactionId}</TableCell>
-                  <TableCell>{transaction.transactionDate}</TableCell>
-                  <TableCell>{transaction.paymentStatus}</TableCell>
-                  <TableCell>{transaction.productId}</TableCell>
-                  <TableCell>{transaction.staffId}</TableCell>
-                  <TableCell>{transaction.memberId}</TableCell>
-                </TableRow>
-              ))}
+              {filteredTransactions
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((transaction) => {
+                  const product = products.find(
+                    (p) => p.productId === transaction.productId
+                  );
+                  const staffMember = staff.find(
+                    (s) => s.staffId === transaction.staffId
+                  );
+                  const member = members.find(
+                    (m) => m.memberId === transaction.memberId
+                  );
+
+                  return (
+                    <TableRow key={transaction.transactionId}>
+                      <TableCell>{transaction.transactionId}</TableCell>
+                      <TableCell>
+                        {transaction.transactionDate
+                          ? new Date(
+                              transaction.transactionDate
+                            ).toLocaleDateString()
+                          : ""}
+                      </TableCell>
+                      <TableCell>
+                        {product ? product.productName : ""}
+                      </TableCell>
+                      <TableCell>
+                        {product ? product.productPrice : ""}
+                      </TableCell>
+                      <TableCell>
+                        {staffMember ? staffMember.name : ""}
+                      </TableCell>
+                      <TableCell>{member ? member.name : ""}</TableCell>
+                      <TableCell>
+                        <TableCell>
+                          <Checkbox
+                            defaultChecked={transaction.paymentStatus}
+                            onChange={() =>
+                              handlePaymentStatusChange(transaction)
+                            }
+                          />
+                        </TableCell>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
             </TableBody>
           </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25, 50]}
+            component="div"
+            count={transactions.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
         </Container>
       </div>
     </>
