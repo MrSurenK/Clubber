088e0d8c1d52da87f333b5d5cccca253b0ec8705
wallgecko <melissa.ho@outlook.com>
2023-08-22 15:08:19 +0800

    dashboard layout

---
+++ front-end/Clubber CRM/src/DashboardComponents/ReceivablesperMonth.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ReceivablesperMonth = () => {
+  return (
+    <div>
+      [This will be a graph for transactions paymentstatus===false sorted by
+      month]
+    </div>
+  );
+};
+
+export default ReceivablesperMonth;
