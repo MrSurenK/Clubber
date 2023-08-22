b568c446464360c5f5cb7f4db8f1a670ded19b3c
oyster2387 <mohamedf2_net@hotmail.com>
2023-08-22 15:03:21 +0800

    Added staffID and memberID validator

--- back-end/src/routers/users.js
+++ back-end/src/routers/users.js
@@ -13,12 +13,16 @@ const {
 
 const checkValid = require("../middleware/checkValid");
 const { auth, authStaff, authManager } = require("../middleware/auth");
+const {
+  validateStaffIdData,
+  validateMemberIdData,
+} = require("../validators/users");
 
 router.get("/staff", authStaff, getAllStaff);
 router.get("/member", authStaff, getAllMember);
 router.get("/all", authManager, getAllUser);
-router.post("/member", auth, checkValid, getMemberById);
-router.post("/staff", authStaff, checkValid, getStaffById);
+router.post("/member", auth, validateMemberIdData, checkValid, getMemberById);
+router.post("/staff", authStaff, validateStaffIdData, checkValid, getStaffById);
 router.patch("/member/:memberId", authStaff, checkValid, patchMember);
 router.patch("/staff/:staffId", authManager, checkValid, patchStaff);
 router.delete("/all/:id", authManager, checkValid, delUser);
