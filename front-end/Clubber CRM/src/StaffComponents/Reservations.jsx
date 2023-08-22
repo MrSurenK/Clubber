597b2608781fc30a0d2a9854a8ab824e5d7d3677
MrSurenK <surenkworkz@gmail.com>
2023-08-23 03:47:02 +0800

    created table for reservations

--- front-end/Clubber CRM/src/StaffComponents/Reservations.jsx
+++ front-end/Clubber CRM/src/StaffComponents/Reservations.jsx
@@ -1,19 +1,18 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import UserContext from "../context/user";
-import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
-
-import {
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  TablePagination,
-} from "@mui/material";
+import Container from "@mui/material/Container";
+import { Paper } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableBody from "@mui/material/TableBody";
+import { TextField } from "@mui/material";
 
 const Reservations = () => {
   // Declare states to store table variables
@@ -22,6 +21,8 @@ const Reservations = () => {
   // For pagination
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [search, setSearch] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchData = useFetch();
 
@@ -45,7 +46,81 @@ const Reservations = () => {
     console.log(reservation);
   }, []);
 
-  return <></>;
+  // const handleChangePage = (event, newPage) => {
+  //   setPage(newPage);
+  // };
+
+  // const handleChangeRowsPerPage = (event) => {
+  //   setRowsPerPage(parseInt(event.target.value, 5));
+  //   setPage(0);
+  // };
+
+  // Search function
+  const handleSearch = () => {
+    return reservation.filter((reserve) => {
+      return (
+        reserve.reservationId.toLowerCase().includes(search) ||
+        reserve.memberId.toLowerCase().includes(search)
+      );
+    });
+  };
+
+  return (
+    <>
+      <Container fixed sx={{ textAlign: "center" }}>
+        <Typography variant="h4">Reservations</Typography>
+        <TextField
+          label="Search Reservation..."
+          variant="outlined"
+          sx={{ mb: 5, width: "40%" }}
+          onChange={(e) => setSearch(e.target.value.toLowerCase)}
+        />
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead sx={{ bgcolor: "#ffc400" }}>
+              <TableRow>
+                {[
+                  "Reservation ID",
+                  "Member ID",
+                  "Date",
+                  "Time",
+                  "Pax",
+                  "Status",
+                ].map((header) => (
+                  <TableCell
+                    sx={{
+                      color: "black",
+                      fontWeight: "700",
+                      fontFamily: "Exo",
+                    }}
+                    key={header}
+                  >
+                    {header}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {handleSearch().map((row, idx) => {
+                return (
+                  <TableRow key={idx}>
+                    <TableCell component="th" scope="row">
+                      {row.reservationId}
+                    </TableCell>
+                    <TableCell>{row.memberId}</TableCell>
+                    <TableCell>{row.reservationDate}</TableCell>
+                    <TableCell>{row.reservationTime}</TableCell>
+                    <TableCell>{row.reservationPax}</TableCell>
+                    <TableCell>{row.reservationStatus}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
+    </>
+  );
 };
 
 export default Reservations;
